31bb819a9c87e352a18c1e0056b8f5dec54d4a72
YAA <yudovanton@yandex.ru>
2022-12-29 23:50:52 +0300

    Визуальная чать стартового окна

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pygame
 from tkinter import *
-
+from GeneralFunctions import terminate, load_image
 root = Tk()
 pygame.init()
 pygame.key.set_repeat(200, 70)
@@ -15,24 +15,6 @@ STEP = 10
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 meny_sprites = pygame.sprite.Group()
-
-
-def load_image(name, color_key=None, cat='data'):
-    fullname = os.path.join(cat, name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error as message:
-        print('Cannot load image:', name)
-        raise SystemExit(message)
-
-    if color_key is not None:
-        image.convert()
-        if color_key == -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-    else:
-        image = image.convert_alpha()
-    return image
 
 
 class Boor(pygame.sprite.Sprite):
@@ -49,24 +31,83 @@ class Boor(pygame.sprite.Sprite):
         self.rect.y = 858 * (HEIGHT / 1020) + 1
 
     def update(self, *args):
-        im = self.image.copy()
         if args and self.rect.collidepoint(args[0].pos):
-            pygame.draw.rect(self.image, pygame.Color('#f7943c'),
-                             (0, 0, self.image.get_width() - 1, self.image.get_height() - 1), 3)
+            pygame.draw.rect(self.image, pygame.Color('#cc5500'),
+                             (0, 0, self.image.get_width(), self.image.get_height()), 5)
         else:
             pygame.draw.rect(self.image, pygame.Color('#6f6677'),
-                             (0, 0, self.image.get_width() - 1, self.image.get_height() - 1), 3)
+                             (0, 0, self.image.get_width(), self.image.get_height()), 5)
 
 
-def terminate():
-    pygame.quit()
-    sys.exit()
+class User(pygame.sprite.Sprite):
+    # Открываем изображение и маштабируем
+    user = load_image("sprite_user.png", cat='Sprite_meny_play')
+    image = pygame.transform.scale(user, ((user.get_width() / 2) * (WIDTH / 1000), (user.get_height() / 1.85) * (HEIGHT / 1000)))
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = User.image
+        self.rect = self.image.get_rect()
+        # Координаты левого верхнего угла с учетом размера экранна
+        self.rect.x = 995 * (WIDTH / 1069) + 1
+        self.rect.y = 15 * (HEIGHT / 1020) + 1
+
+    def update(self, *args):
+        if args and self.rect.collidepoint(args[0].pos):
+            pygame.draw.rect(self.image, pygame.Color('#3b83bd'),
+                             (0, 0, self.image.get_width(), self.image.get_height() + 1), 3)
+        else:
+            pygame.draw.rect(self.image, pygame.Color('#7da4c5'),
+                             (0, 0, self.image.get_width(), self.image.get_height() + 1), 3)
+
+
+class Top(pygame.sprite.Sprite):
+    # Открываем изображение и маштабируем
+    top = load_image("sprite_top.png", cat='Sprite_meny_play')
+    image = pygame.transform.scale(top, ((top.get_width() / 2) * (WIDTH / 1000), (top.get_height() / 1.87) * (HEIGHT / 1000)))
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = Top.image
+        self.rect = self.image.get_rect()
+        # Координаты левого верхнего угла с учетом размера экранна
+        self.rect.x = 905 * (WIDTH / 1069) + 1
+        self.rect.y = 15 * (HEIGHT / 1020) + 1
+
+    def update(self, *args):
+        if args and self.rect.collidepoint(args[0].pos):
+            pygame.draw.rect(self.image, pygame.Color('#3b83bd'),
+                             (0, 0, self.image.get_width(), self.image.get_height() + 1), 3)
+        else:
+            pygame.draw.rect(self.image, pygame.Color('#7da4c5'),
+                             (0, 0, self.image.get_width(), self.image.get_height() + 1), 3)
+
+
+class ExitСross(pygame.sprite.Sprite):
+    # Открываем изображение и маштабируем
+    top = load_image("exit_cross.png", cat='Sprite_meny_play')
+    image = pygame.transform.scale(top, ((top.get_width() / 1.1) * (WIDTH / 1069), (top.get_height() / 1.1) * (HEIGHT / 1020)))
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = ExitСross.image
+        self.rect = self.image.get_rect()
+        # Координаты левого верхнего угла с учетом размера экранна
+        self.rect.x = 5 * (WIDTH / 1069) + 1
+        self.rect.y = 5 * (HEIGHT / 1020) + 1
+
+    def update(self, *args):
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
+            terminate()
 
 
 def start_screen():
     fon = pygame.transform.scale(load_image('meny.jpg', cat='Sprite_meny_play'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     Boor(meny_sprites)
+    User(meny_sprites)
+    Top(meny_sprites)
+    ExitСross(meny_sprites)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -74,10 +115,8 @@ def start_screen():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     terminate()
-            if event.type == pygame.MOUSEMOTION:
+            if event.type == pygame.MOUSEMOTION or event.type == pygame.MOUSEBUTTONDOWN:
                 meny_sprites.update(event)
-            # elif event.type == pygame.MOUSEBUTTONDOWN:
-            #     return  # начинаем игру
         meny_sprites.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
