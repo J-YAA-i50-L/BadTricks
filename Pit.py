39b266eb3df4ab795597d7c0fc6f0aad6dbd5b04
YAA <yudovanton@yandex.ru>
2023-01-26 12:41:53 +0300

    Питер не ходит через стены

--- Pit.py
+++ Pit.py
@@ -52,7 +52,7 @@ class Pit(pygame.sprite.Sprite):
                     self.dmove = False
         elif args and args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_RIGHT and not self.dmove and not self.umove:
             print(info_subject()[2])
-            if [self.rect.x // 10 * 10 + 5 + 40 * (WIDTH / 1750) // 10 * 10,
+            if [(self.rect.x // 10 * 10 + 5) + (40 * (WIDTH / 1750) // 10 * 10),
                (self.rect.y - 5 * (HEIGHT / 850)) // 10 * 10] not in info_subject()[2]:
                 self.rmove = True
                 self.lmove = False
@@ -61,8 +61,8 @@ class Pit(pygame.sprite.Sprite):
                 self.rmove = False
                 self.lmove = False
         elif args and args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_LEFT and not self.dmove and not self.umove:
-            if [self.rect.x // 10 * 10 + 5 - 20 * (WIDTH / 1750) // 10 * 10,
-               (self.rect.y - 5 * (HEIGHT / 850)) // 10 * 10] not in info_subject()[2]:
+            if [self.rect.x // 10 * 10 + 5 - (30 * (WIDTH / 1750) // 10 * 10),
+               (self.rect.y - (5 * (HEIGHT / 850)) // 10 * 10)] not in info_subject()[2]:
                 self.rmove = False
                 self.lmove = True
                 self.rect = self.rect.move(-10, 0)
